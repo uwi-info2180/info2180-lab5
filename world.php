74339e6dbf4c2fcfed848a1781db84b8daa5f5e1
Yannick Lyn Fatt <yannick.lynfatt@gmail.com>
2019-11-13 15:42:43 +0000

    Change the way HTML is being output in PHP

--- world.php
+++ world.php
@@ -1,18 +1,16 @@
 <?php
-
 $host = getenv('IP');
-$username = getenv('C9_USER');
+$username = 'lab7_user';
 $password = '';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
 $stmt = $conn->query("SELECT * FROM countries");
-
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo '<ul>';
-foreach ($results as $row) {
-  echo '<li>' . $row['name'] . ' is ruled by ' . $row['head_of_state'] . '</li>';
-}
-echo '</ul>';
+?>
+<ul>
+<?php foreach ($results as $row): ?>
+  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+<?php endforeach; ?>
+</ul>
